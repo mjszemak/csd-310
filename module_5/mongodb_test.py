eef1fa8ee986f254bf8181ebd27c306110116d8b
mjszemak <mjszemak@gmail.com>
2022-07-10 21:12:14 -0400

    updated mongodb_test.py and pytech_queries.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -1,10 +1,22 @@
+import msvcrt
 from os import system
 from pymongo import MongoClient
-url = "mongodb+srv://admin:admin@cluster0.qzqbr2z.mongodb.net/pytech";
-client = MongoClient(url)
-db = client.pytech
-system('cls')
-print('-- Pytech Collections List --')
-print(db.list_collection_names())
-print('\n')
-system('PAUSE')
+
+def main():
+    url = "mongodb+srv://admin:admin@cluster0.qzqbr2z.mongodb.net/pytech";
+    client = MongoClient(url)
+    db = client.pytech
+
+    system('cls')
+    print('-- Pytech Collections List --')
+    print(db.list_collection_names())
+    print('\nEnd of program, press any key to exit...')
+
+    while (True):
+        if msvcrt.kbhit():
+            msvcrt.getch()
+            break
+    
+
+if __name__ == "__main__":
+    main()
--- module_5/pytech_queries.py
+++ module_5/pytech_queries.py
@@ -2,34 +2,38 @@ import msvcrt
 from os import system
 from pymongo import MongoClient
 
-client = MongoClient("mongodb+srv://admin:admin@cluster0.qzqbr2z.mongodb.net/pytech",)
+def main():
+    client = MongoClient("mongodb+srv://admin:admin@cluster0.qzqbr2z.mongodb.net/pytech")
 
-db = client["mydb"]
+    db = client["mydb"]
 
-pytech = db["PyTech"]
+    pytech = db["PyTech"]
 
-docs = pytech.find()
+    docs = pytech.find()
 
-system('cls')
+    system('cls')
 
-print('-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --')
-for doc in docs:
+    print('-- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --')
+    for doc in docs:
+        print("Student ID: %s" % (doc["student_id"]))
+        print("First Name: %s" % (doc["first_name"]))
+        print("Last Name: %s" % (doc["last_name"]))
+        print('\n')
+        
+
+    print('-- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY --')
+    doc = pytech.find_one({"student_id" : "1007"})
     print("Student ID: %s" % (doc["student_id"]))
     print("First Name: %s" % (doc["first_name"]))
     print("Last Name: %s" % (doc["last_name"]))
     print('\n')
-    
 
-print('-- DISPLAYING STUDENT DOCUMENT FROM find_one() QUERY --')
-doc = pytech.find_one({"student_id" : "1007"})
-print("Student ID: %s" % (doc["student_id"]))
-print("First Name: %s" % (doc["first_name"]))
-print("Last Name: %s" % (doc["last_name"]))
-print('\n')
+    print('\nEnd of program, press any key to exit...')
 
-print('\nEnd of program, press any key to exit...')
+    while (True):
+        if msvcrt.kbhit():
+            msvcrt.getch()
+            break
 
-while (True):
-	if msvcrt.kbhit():
-		msvcrt.getch()
-		break
+if __name__ == "__main__":
+    main()
